0e698a5392fe489546a1b44035be296cc658a357
Serdar Salim <slmxyz@gmail.com>
2026-04-17 15:33:01 +0800

    Keep app running in background when window is closed, add menu bar icon

--- Sources/LocalhostApp/App.swift
+++ Sources/LocalhostApp/App.swift
@@ -14,12 +14,46 @@ struct LocalhostApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.regular)
         NSApp.activate(ignoringOtherApps: true)
+        setupMenuBarIcon()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        true
+        false
+    }
+
+    private func setupMenuBarIcon() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "network", accessibilityDescription: "Localhost")
+            button.action = #selector(toggleWindow)
+            button.target = self
+        }
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Show Localhost", action: #selector(showWindow), keyEquivalent: ""))
+        menu.addItem(.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem?.menu = menu
+    }
+
+    @objc private func toggleWindow() {
+        if let window = NSApp.windows.first(where: { $0.isVisible }) {
+            window.orderOut(nil)
+        } else {
+            showWindow()
+        }
+    }
+
+    @objc private func showWindow() {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+        if let window = NSApp.windows.first {
+            window.makeKeyAndOrderFront(nil)
+        }
     }
 }
